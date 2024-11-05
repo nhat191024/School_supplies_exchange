cb3063d44565ccd222a000a87ae16e61881a9ada
nhat191024 <richberchannel01@gmail.com>
2024-11-05 14:58:26 +0700

    Fix: correct comment formatting in router configuration

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -22,7 +22,7 @@ export default route(function (/* { store, ssrContext } */) {
 
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
+    // quasar.conf.js -> build -> publicPath`
     history: createHistory(process.env.VUE_ROUTER_BASE)
   })
 
