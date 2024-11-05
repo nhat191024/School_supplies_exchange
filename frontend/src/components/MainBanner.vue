4b0c444618b04d58177c33ee55cbd013b71dfb32
nhat191024 <richberchannel01@gmail.com>
2024-11-05 14:55:40 +0700

    Feat: add MainBanner component with carousel functionality

---
+++ frontend/src/components/MainBanner.vue
@@ -0,0 +1,17 @@
+<template>
+    <q-carousel animated v-model="slide" infinite :autoplay="autoplay" height="150px">
+        <q-carousel-slide :name="1" img-src="/images/slide/slider-1.jpg" />
+        <q-carousel-slide :name="2" img-src="/images/slide/slider-2.png" />
+    </q-carousel>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const slide = ref(0)
+const autoplay = ref(true)
+
+defineOptions({
+    name: 'MainBanner'
+})
+</script>
