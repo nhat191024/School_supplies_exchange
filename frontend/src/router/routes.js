23dd90463e541b1eb4d5f9e3b5dd98281626922c
nhat191024 <richberchannel01@gmail.com>
2024-12-04 16:14:41 +0700

    refactor: update routes to include dynamic parameters for product details and add new routes for payment and history pages

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -6,7 +6,9 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue'), },
       { path: '/profile', component: () => import('pages/ProfilePage.vue'), },
       { path: '/post-product', component: () => import('pages/AddProductPage.vue'), },
-      { path: '/product/', component: () => import('pages/ProductDetail.vue'), },
+      { path: '/product/:id', component: () => import('pages/ProductDetail.vue'), },
+      { path: '/paid/:id', component: () => import('pages/PayPage.vue'), },
+      { path: '/history/:type', component: () => import('pages/HistoryPage.vue') },
     ]
   },
 
