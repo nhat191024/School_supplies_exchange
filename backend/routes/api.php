bc17f30616f6b0ec160e7723f063ae04216b3854
nhat191024 <richberchannel01@gmail.com>
2025-04-25 23:25:14 +0700

    feat: add endpoint for retrieving products posted by the user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,10 +17,15 @@ Route::get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
+
 Route::apiResource('users', UserController::class);
 Route::post('/users/profile', [UserController::class, 'update'])->middleware('auth:sanctum');
+
 Route::apiResource('categories', CategoryController::class);
+
 Route::apiResource('products', ProductController::class);
-// Route::apiResource('bills', BillController::class);
+Route::get('/products/user/postProduct', [ProductController::class, 'postByUser'])->middleware('auth:sanctum');
+
 Route::get('/bills/{type}', [BillController::class, 'index'])->middleware('auth:sanctum');
+
 Route::apiResource('reviews', ReviewController::class);
