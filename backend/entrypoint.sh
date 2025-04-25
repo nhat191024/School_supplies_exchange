477ee09d2e1b34e666966b80c600dd149c2f4f79
nhat191024 <richberchannel01@gmail.com>
2025-04-26 06:52:00 +0700

    fix: ensure proper permissions and ownership for public directory in entrypoint script

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,9 +6,11 @@ mkdir -p /var/www/html/storage/framework/{sessions,views,cache}
 chmod -R 775 /var/www/html/storage
 chmod -R 775 /var/www/html/bootstrap
 chmod -R 775 /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/public
 chown -R www-data:www-data /var/www/html/storage
 chown -R www-data:www-data /var/www/html/bootstrap
 chown -R www-data:www-data /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/html/public
 
 php artisan optimize:clear
 
