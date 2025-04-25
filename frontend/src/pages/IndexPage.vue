da180f100fe1951ae061cbb59e92233320573866
nhat191024 <richberchannel01@gmail.com>
2025-04-26 02:59:30 +0700

    fix: update category initialization and enhance category fetching on mount

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -5,17 +5,27 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import MainBanner from 'src/components/MainBanner.vue';
 import MainCategory from 'src/components/MainCategory.vue';
 import MainProduct from 'src/components/MainProduct.vue';
 
 const category = ref([
-  { name: 'Hộp bút', icon: 'home_repair_service' },
+  { name: 'Tất cả', icon: 'home', id: 0 },
+  { name: 'Hộp bút', icon: 'home_repair_service', id: 1 },
   { name: 'Vở', icon: 'menu_book' },
   { name: 'Bút', icon: 'edit' },
   { name: 'Cặp', icon: 'work' }
 ]);
+
+onMounted(() => {
+  fetch('http://192.168.1.4:8000/api/categories')
+    .then(response => response.json())
+    .then(data => {
+      category.value = data;
+    })
+    .catch(error => console.error('Error fetching categories:', error));
+});
 
 defineOptions({
   name: 'IndexPage'
