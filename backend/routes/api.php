3457e9ee0ce9f13d7693748b7d17317fe68b4c89
nhat191024 <richberchannel01@gmail.com>
2025-04-26 02:19:43 +0700

    feat: refactor product routes to define individual endpoints for better clarity and control

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -23,7 +23,12 @@ Route::post('/users/profile', [UserController::class, 'update'])->middleware('au
 
 Route::apiResource('categories', CategoryController::class);
 
-Route::apiResource('products', ProductController::class);
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+Route::post('/products/update/{product}', [ProductController::class, 'update']);
+Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
 Route::get('/products/user/postProduct', [ProductController::class, 'postByUser'])->middleware('auth:sanctum');
 
 Route::get('/bills/{type}', [BillController::class, 'index'])->middleware('auth:sanctum');
