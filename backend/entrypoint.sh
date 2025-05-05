f98601f76ee776a4e11f20409e4c1a258bd38837
nhat191024 <richberchannel01@gmail.com>
2025-05-05 19:55:03 +0700

    fix: simplify permissions and ownership settings in entrypoint script

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Đảm bảo thư mục framework cache tồn tại
 mkdir -p /var/www/html/storage/framework/{sessions,views,cache}
 
-chmod -R 775 /var/www/html/storage
-chmod -R 775 /var/www/html/bootstrap
-chmod -R 775 /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/public
-chown -R www-data:www-data /var/www/html/storage
-chown -R www-data:www-data /var/www/html/bootstrap
-chown -R www-data:www-data /var/www/html/bootstrap/cache
-chown -R www-data:www-data /var/www/html/public
+chmod -R 775 /var/www/html
+chown -R www-data:www-data /var/www/html
 
 php artisan optimize:clear
 
