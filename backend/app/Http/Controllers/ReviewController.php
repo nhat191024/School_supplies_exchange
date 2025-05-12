bf90e97adfb83a99f865b2f72910f6a9ad17b0d4
nhat191024 <richberchannel01@gmail.com>
2025-05-12 13:42:12 +0700

    refactor: simplify store method in ReviewController by using request data directly

--- backend/app/Http/Controllers/ReviewController.php
+++ backend/app/Http/Controllers/ReviewController.php
@@ -4,38 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Review;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
 {
     public function store(Request $request)
     {
-        $review = Review::create($request->all());
-        return response()->json($review, 201);
-    }
+        $userId = Auth::user()->id;
+        Review::create([
+            'bill_id' => $request->bill_id,
+            'product_id' => $request->product_id,
+            'buyer_id' => $userId,
+            'seller_id' => $request->seller_id,
+            'rating' => $request->rating,
+            'comment' => $request->comment,
+        ]);
 
-    public function index()
-    {
-        $reviews = Review::all();
-        return response()->json($reviews);
-    }
-
-    public function show($id)
-    {
-        $review = Review::findOrFail($id);
-        return response()->json($review);
-    }
-
-    public function update(Request $request, $id)
-    {
-        $review = Review::findOrFail($id);
-        $review->update($request->all());
-        return response()->json($review);
-    }
-
-    public function destroy($id)
-    {
-        $review = Review::findOrFail($id);
-        $review->delete();
-        return response()->json(null, 204);
+        return response()->json('success', 201);
     }
 }
